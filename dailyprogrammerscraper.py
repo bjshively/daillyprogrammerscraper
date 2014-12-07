7cc92d5afd39b404859a579a3ddbcaeb88c839d5
Bradley Shively <bshively@gmail.com>
2014-12-07 12:16:24 -0500

    Cleaned up HTML strings, refactored to add main()

--- dailyprogrammerscraper.py
+++ dailyprogrammerscraper.py
@@ -6,29 +6,27 @@ import urllib2
 import re
 import HTMLParser
 
-#Create a directory to save the output files in
-target_dir = 'dps'
-if not (os.path.exists(target_dir)):
-  os.mkdir(target_dir)
-
-#Prepare the masterlist file
-masterlist = open(os.path.join(target_dir, "dps.html"), 'w+')
-masterlist.write('<html>\n<body>\n<ul>')
-masterlist.close()
-
-challenges = []
-
-def render_post_html(post):
+def render_post_html(post, target_dir):
 	if not post.selftext_html: return
 
 	#setup
 	html_parser = HTMLParser.HTMLParser()
-	htmlbegin = "<html>\n<body>\n<h1>"
-	htmlend = "</body>\n</html>"
+
+	htmlbegin = """<html>
+	<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	</head>
+	<body>
+	<div class="container">
+	<h1>"""
+
+	htmlend = """</div>
+	</body>
+	</html>"""
 
 	#Add an entry to the master list for the challenge
 	masterlist = open(os.path.join(target_dir, "dps.html"), 'a')
-	masterlist.write('<li><a href="' + post.id + '.html">'+ post.title.encode('ascii', 'ignore') + '</a></li>\n')
+	masterlist.write('<li><a href="%s.html">%s</a></li>\n' % (post.id, post.title.encode('ascii', 'ignore')))
 	masterlist.close()
 
 	#Create and save the challenge page
@@ -36,50 +34,58 @@ def render_post_html(post):
 	file.write(htmlbegin + post.title.encode('ascii', 'ignore') + "</h1>" + html_parser.unescape(post.selftext_html).encode('ascii', 'ignore') + htmlend)
 	file.close()
 
-#Download the /r/dailyprogrammer feed
-print "Getting posts from reddit..."
-r = praw.Reddit(user_agent='User-Agent: dailyprogrammerscraper v.1 by /u/bjshively')
-posts = r.get_subreddit('dailyprogrammer').search('challenge', limit=None)
+def get_challenges():
+	#Create a directory to save the output files in
+	target_dir = 'dps'
+	if not (os.path.exists(target_dir)):
+	  os.mkdir(target_dir)
 
-print "Making list of challenges..."
-for post in posts:
-	challenges.append(post)
+	#HTML setup for masterlist
+	ml_html_head="""<html>
+	<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	</head>
+	<body>
+	<div class="container">
+	<h1>/r/dailyprogrammer challenge directory</h1>
+	<ul>
+	"""
 
-#Sort by creation date
-print "Sorting challenges..."
-challenges.sort(key=lambda x: x.created_utc)
+	#Prepare the masterlist file
+	masterlist = open(os.path.join(target_dir, "dps.html"), 'w+')
+	masterlist.write(ml_html_head)
+	masterlist.close()
 
-for challenge in challenges:
-	print "Rendering " + challenge.id
-	render_post_html(challenge)
+	challenges = []
 
-masterlist = open(os.path.join(target_dir, "dps.html"), 'a')
-masterlist.write('</ul>\n</body>\n</html>')
-masterlist.close()
+	#Download the /r/dailyprogrammer feed
+	print "Getting posts from reddit..."
+	r = praw.Reddit(user_agent='User-Agent: dailyprogrammerscraper v.1 by /u/bjshively')
+	posts = r.get_subreddit('dailyprogrammer').search('challenge', limit=None)
 
-#Outputs a single challenge in decoded HTML
-#print render_post_html(samplepost)
+	print "Making list of challenges..."
+	for post in posts:
+		if(re.search("\d+/\d+/\d+", post.title)):
+			challenges.append(post)
 
-#for post in submissions:
-#	challenge_title_dict[post.created_utc] = str(post)
-#	challenge_url_dict[post.created_utc] = post.url
-	#TODO - sort challenges by challenge # or date
+	#Sort by creation date
+	print "Sorting challenges..."
+	challenges.sort(key=lambda x: x.created_utc)
 
-	#ordered_challenges.append(str(post), key=post.created_utc)
+	for challenge in challenges:
+		print "Rendering " + challenge.id
+		render_post_html(challenge, target_dir)
 
-#ordered_challenges = sorted(ordered_challenges, key=created_utc)
+	#Wrapup the master index, dps.html
+	masterlist = open(os.path.join(target_dir, "dps.html"), 'a')
+	masterlist.write('</ul>\n</div></body>\n</html>')
+	masterlist.close()
 
-#post_dates = challenge_title_dict.keys()
-#post_dates = sorted(post_dates)
+def main():
+	print "#" * 40
+	print "/r/dailyprogrammer challenge scraper"
+	print "#" * 40
+	get_challenges()
 
-#for e in post_dates:
-#	print challenge_title_dict[e]
-#	print challenge_url_dict[e]
-
-#Identify which posts are challenges
-
-#Download each of the challenges
-
-#Save the challenge to a separate HTML document
-
-#Build an index of the downloaded challenges
+if __name__ == '__main__':
+  main()
